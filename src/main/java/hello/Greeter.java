1160f630b6fc6d8752d920033c223e1012299b54
testingcloud1994 <110455479+testingcloud1994@users.noreply.github.com>
2024-04-14 19:55:25 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,7 +1,7 @@
 package hello;
 
 public class Greeter {
-    public String sayHello() {
-        return "Deployment successful";
-    }
+  public String sayHello() {
+    return "Hello world!";
+  }
 }
